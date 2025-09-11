f81165e6324e03cc4399b7a829185f1ea67e46c7
huzzeyfh <hudzaifah7103@gmail.com>
2025-09-11 21:04:56 +0700

    Add result of choices for each players

--- script.js
+++ script.js
@@ -3,32 +3,57 @@ const rockButton = document.querySelector(".rock");
 const paperButton = document.querySelector(".paper");
 const scissorButton = document.querySelector(".scissor");
 
+
 //result variables
+const humanChoice = document.querySelector(".human-choice");
+const computerChoice = document.querySelector(".computer-choice");
 const result = document.querySelector(".result");
 const humanPoint = document.querySelector(".human-point");
 const computerPoint = document.querySelector(".computer-point");
 
 
+// // Initial score
+let humanScore = 0;
+let computerScore = 0;
+let humanChoiceInit = "";
+
 //click action button
 rockButton.addEventListener("click", () => {
   playRound("Rock", getComputerChoice());
+  humanChoiceInit = "Rock";
+
+  //choice
+  humanChoice.textContent = ("Your choice: " + (`${humanChoiceInit}`));
+  computerChoice.textContent = ("Computer choice: " + (`${getComputerChoice()}`));
+
+  //score
   humanPoint.textContent = ("Your score: " + (`${humanScore}`)); 
-  computerPoint.textContent = ("Your score: " + (`${computerScore}`)); 
+  computerPoint.textContent = ("Computer score: " + (`${computerScore}`)); 
+  
 });
 
 paperButton.addEventListener("click", () => {
   playRound("Paper", getComputerChoice());
+  humanChoiceInit = "Paper";
+  
+  //choice
+  humanChoice.textContent = ("Your choice: " + (`${humanChoiceInit}`));
+  computerChoice.textContent = ("Computer choice: " + (`${getComputerChoice()}`));
+
+  //score
   humanPoint.textContent = ("Your score: " + (`${humanScore}`)); 
   computerPoint.textContent = ("Your score: " + (`${computerScore}`)); 
 });
 
 scissorButton.addEventListener("click", () => {
   playRound("Scissor", getComputerChoice());
-  if playRound(humanWin) = true {
-    result.textContent = "You win!";
-  } else {
-    result.textContent = "You lose!";
-  }
+  humanChoiceInit = "Scissor";
+
+  //choice
+  humanChoice.textContent = ("Your choice: " + (`${humanChoiceInit}`));
+  computerChoice.textContent = ("Computer choice: " + (`${getComputerChoice()}`));
+
+  //score
   humanPoint.textContent = ("Your score: " + (`${humanScore}`)); 
   computerPoint.textContent = ("Your score: " + (`${computerScore}`)); 
 });
@@ -37,30 +62,27 @@ scissorButton.addEventListener("click", () => {
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === "Rock" && computerChoice === "Paper") {
     computerScore++;
-    return humanWin;
+    return "computerWin";
     
   } else if (humanChoice === "Rock" && computerChoice === "Scissor") {
     humanScore++;
-    return humanWin;
+    return "humanWin";
 
   } else if (humanChoice === "Scissor" && computerChoice === "Rock") {
     computerScore++;
-    return humanWin;
+    return "computerWin";
 
   } else if (humanChoice === "Scissor" && computerChoice === "Paper") {
     humanScore++;
-    return humanWin;
+    return "humanWin";
 
   } else if (humanChoice === "Paper" && computerChoice === "Scissor") {
     computerScore++;
-    return humanWin;
+    return "computerWin";
 
   } else if (humanChoice === "Paper" && computerChoice === "Rock") {
     humanScore++;
-    return humanWin;
-
-  } else if (humanChoice === null) {
-    console.log("Invalid")
+    return "humanWin";
 
   } else {
     console.log("Draw!")
@@ -84,9 +106,6 @@ function getComputerChoice() {
   }
 }
 
-// // Initial score
-let humanScore = 0;
-let computerScore = 0;
 
 //After 5 rounds
 console.log(`Final score -> Human = ${humanScore}, Computer = ${computerScore}`);
